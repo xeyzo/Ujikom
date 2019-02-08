46d52635b76ed236487ecab1a844dc569d9320ce
xeyzo <schneidergw@yahoo.com>
2019-02-08 14:15:29 +0700

    1

--- aplikasi/views.py
+++ aplikasi/views.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Pendaftaran
+
 
 def index (request):
     return HttpResponse ("Hello world")
